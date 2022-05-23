4b69e2770e42e2bdb5afbacadd1c716bb8747536
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-05-23 18:55:50 +0800

    Adjusted Styling on Services Center

--- src/components/extracontent/services/servicesparttwo.js
+++ src/components/extracontent/services/servicesparttwo.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import './servicesparttwo.css';
-import ServiceCardCompile from './servicecardcompile';
+import React, { Component } from "react";
+import "./servicesparttwo.css";
+import ServiceCardCompile from "./servicecardcompile";
 
 class ServicesPartTwo extends Component {
-    render() { 
-        return ( 
-            <div className="servicesPartTwoWrapper">
-                <div className="servicesPartTwoContent"> 
-                    <h1>Our Services</h1>
-                </div>
-                <ServiceCardCompile />
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div className="servicesPartTwoWrapper">
+        <div className="servicesPartTwoContent">
+          <h1>Our Services</h1>
+        </div>
+        <ServiceCardCompile />
+      </div>
+    );
+  }
 }
- 
+
 export default ServicesPartTwo;
